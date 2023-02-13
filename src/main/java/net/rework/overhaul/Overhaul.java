4a6796047de8c502074cdde8fd1bd02714f2f7e7
Adam Garza <boxeigit@gmail.com>
2023-02-12 21:37:11 -0600

    Initial commit

---
+++ src/main/java/net/rework/overhaul/Overhaul.java
@@ -0,0 +1,15 @@
+package net.rework.overhaul;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Overhaul implements ModInitializer {
+	public static final String MOD_ID = "overhaul";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
